f6f7ba6facfd923381dd4b84da374204de8f7c0d
RaspberryPi fumisoro <fumipumisecky@gmail.com>
2022-07-02 08:38:34 +0900

    [update] update NodeJS to v16

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 // http://bit-trade-one.co.jp/product/module/adrsir/
 let http = require("http");
-let RtmClient = require("@slack/client").RtmClient;
-let RTM_EVENTS = require("@slack/client").RTM_EVENTS;
-let CLIENT_EVENTS = require("@slack/client").CLIENT_EVENTS;
-let irFreq = require("./irFreq");
+const { RTMClient } = require('@slack/rtm-api');
 let secret = require("./secret");
 const Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
 const Net = require("net");
 const tvSocketClient = new Net.Socket();
-const cronJob = require("cron").CronJob;
 
 function sleep(time) {
   return new Promise((resolve, reject) => {
@@ -19,7 +15,7 @@ function sleep(time) {
 }
 
 function sendSignalToTV(command) {
-  tvSocketClient.connect(secret.tvPort(), secret.tvHost(), async function(
+  tvSocketClient.connect(secret.tvPort(), secret.tvHost(), async function (
     data
   ) {
     console.log("tvSocketClient connected");
@@ -32,13 +28,13 @@ function sendSignalToTV(command) {
   });
 }
 
-tvSocketClient.on("close", function() {
+tvSocketClient.on("close", function () {
   console.log("tvSocketClient closed");
   tvSocketClient.end();
   tvSocketClient.destroy();
 });
 
-tvSocketClient.on("error", function() {
+tvSocketClient.on("error", function () {
   console.log("tvSocketClient error");
   tvSocketClient.end();
   tvSocketClient.destroy();
@@ -48,246 +44,131 @@ function sendSignal(pinNum) {
   const pin = new Gpio(pinNum, "out");
   pin.writeSync(1);
   pin.writeSync(0);
-  pin.unexport();
+  pin.writeSync(1);
 }
 
 let token = secret.slackToken();
 
-let rtm = new RtmClient(token, { logLevel: "debug" });
+let rtm = new RTMClient(token, { logLevel: "debug" });
 
-let myHouseGroup;
+let myHouseGroupId = secret.slackMyHouseGroupId;
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, rtmStartData => {
-  for (const c of rtmStartData.groups) {
-    if (c.name === "my_house") {
-      myHouseGroup = c.id;
-    }
-  }
-  console.log(
-    `Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`
-  );
-});
-
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log(message);
-  switch (message.channel) {
-    case myHouseGroup:
-      postAtMyHouse(message);
-      break;
+rtm.on('message', event => {
+  // my-houseチャンネルの投稿
+  if (event.channel === myHouseGroupId) {
+    onMessageReceived(event.text);
   }
 });
 
-let job = new cronJob({
-  cronTime: "00 30 6 * * *",
-  onTick: () => {
-    sendSignal(27);
-  },
-  start: false,
-  timeZone: "Asia/Tokyo"
-});
-//job.start();
-
-let postAtMyHouse = async message => {
-  if (message.username == "IFTTT") {
-    message.text = message.attachments[0].pretext.replace(/\s/g, "");
-  }
-  if (message.text.match(/.*電気.*つけて.*/)) {
-    //irRequestPost(irFreq.lightOn());
+let onMessageReceived = async message => {
+  // if (message.username == "IFTTT") {
+  //   message.text = message.attachments[0].pretext.replace(/\s/g, "");
+  // }
+  if (message.match(/.*エアコン.*つけて.*/)) {
     // スイッチ1
     sendSignal(4);
-    rtm.sendMessage("電気つける!", myHouseGroup);
+    rtm.sendMessage("エアコンつける!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*電気.*消して.*/)) {
-    //irRequestPost(irFreq.lightOff());
+  if (message.match(/.*エアコン.*消して.*/)) {
     // スイッチ2
     sendSignal(17);
-    rtm.sendMessage("電気消す!", myHouseGroup);
+    rtm.sendMessage("エアコン消す!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*暖房.*つけて.*/)) {
-    //irRequestPost(irFreq.coolOn());
-    // スイッチ3
-    sendSignal(27);
-    rtm.sendMessage("暖房つける!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*冷房.*つけて.*/)) {
-    //irRequestPost(irFreq.coolOn());
-    // スイッチ5
-    sendSignal(5);
-    rtm.sendMessage("冷房つける!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*エアコン.*消して.*/)) {
-    //irRequestPost(irFreq.airaConOff());
-    // スイッチ4
-    sendSignal(18);
-    rtm.sendMessage("エアコン消す!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*テレビ.*つけて.*/)) {
+  if (message.match(/.*テレビ.*つけて.*/)) {
     sendSignalToTV("POWR1   \n");
-    rtm.sendMessage("テレビの電源いじってみる!", myHouseGroup);
+    rtm.sendMessage("テレビの電源いじってみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*テレビ.*消して.*/)) {
+  if (message.match(/.*テレビ.*消して.*/)) {
     sendSignalToTV("POWR0   \n");
-    rtm.sendMessage("テレビの電源いじってみる!", myHouseGroup);
+    rtm.sendMessage("テレビの電源いじってみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*(チャンネル|ちゃんねる).*次.*/)) {
+  if (message.match(/.*(チャンネル|ちゃんねる).*次.*/)) {
     //irRequestPost(irFreq.tvChannelNext());
     sendSignalToTV("CHUP    \n");
-    rtm.sendMessage("チャンネルかえる!", myHouseGroup);
+    rtm.sendMessage("チャンネルかえる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*(チャンネル|ちゃんねる).*前.*/)) {
+  if (message.match(/.*(チャンネル|ちゃんねる).*前.*/)) {
     //irRequestPost(irFreq.tvChannelPrev());
     sendSignalToTV("CHDW    \n");
-    rtm.sendMessage("チャンネルかえる!", myHouseGroup);
+    rtm.sendMessage("チャンネルかえる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*音量.*上.*/)) {
-    irRequestPost(irFreq.tvVolumeUp());
-    rtm.sendMessage("音量上げてみる!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*音量.*下.*/)) {
-    irRequestPost(irFreq.tvVolumeDown());
-    rtm.sendMessage("音量下げてみる!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*入力切替.*/)) {
-    irRequestPost(irFreq.tvInputModeChange());
-    rtm.sendMessage("入力切替してみる!", myHouseGroup);
-    await sleep(1000);
-  }
-  if (message.text.match(/.*NHK.*/)) {
+  if (message.match(/.*NHK.*/)) {
     // irRequestPost(irFreq.tvNHKSogo());
     sendSignalToTV("CTBD011 \n");
-    rtm.sendMessage("NHKにしてみる!", myHouseGroup);
+    rtm.sendMessage("NHKにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*Eテレ.*/)) {
+  if (message.match(/.*Eテレ.*/)) {
     //irRequestPost(irFreq.tvNHKEtele());
     sendSignalToTV("CTBD021 \n");
-    rtm.sendMessage("Eテレにしてみる!", myHouseGroup);
+    rtm.sendMessage("Eテレにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*TVK.*/)) {
+  if (message.match(/.*TVK.*/)) {
     //irRequestPost(irFreq.tvTVK());
     sendSignalToTV("CTBD31 \n");
-    rtm.sendMessage("TVKにしてみる!", myHouseGroup);
+    rtm.sendMessage("TVKにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*日テレ.*/)) {
+  if (message.match(/.*日テレ.*/)) {
     //irRequestPost(irFreq.tvNipponTv());
     sendSignalToTV("CTBD041 \n");
-    rtm.sendMessage("日テレにしてみる!", myHouseGroup);
+    rtm.sendMessage("日テレにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*テレ朝.*/)) {
+  if (message.match(/.*テレ朝.*/)) {
     //irRequestPost(irFreq.tvTvAsahi());
     sendSignalToTV("CTBD051 \n");
-    rtm.sendMessage("テレ朝にしてみる!", myHouseGroup);
+    rtm.sendMessage("テレ朝にしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*TBS.*/)) {
+  if (message.match(/.*TBS.*/)) {
     //irRequestPost(irFreq.tvTBS());
     sendSignalToTV("CTBD061 \n");
-    rtm.sendMessage("TBSにしてみる!", myHouseGroup);
+    rtm.sendMessage("TBSにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*テレ東.*/)) {
+  if (message.match(/.*テレ東.*/)) {
     //irRequestPost(irFreq.tvTvTokyo());
     sendSignalToTV("CTBD071 \n");
-    rtm.sendMessage("テレ東にしてみる!", myHouseGroup);
+    rtm.sendMessage("テレ東にしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*フジテレビ.*/)) {
+  if (message.match(/.*フジテレビ.*/)) {
     sendSignalToTV("CTBD081 \n");
-    rtm.sendMessage("フジテレビにしてみる!", myHouseGroup);
+    rtm.sendMessage("フジテレビにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*Tokyo.*MX.*/)) {
+  if (message.match(/.*Tokyo.*MX.*/)) {
     //irRequestPost(irFreq.tvTokyoMX());
     sendSignalToTV("CTBD091 \n");
-    rtm.sendMessage("TokyoMXにしてみる!", myHouseGroup);
+    rtm.sendMessage("TokyoMXにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*(スイッチ)|(ps4)|(PS4).*/)) {
+  if (message.match(/.*(スイッチ)|(ps4)|(PS4).*/)) {
     //inputModeInterval(3);
     sendSignalToTV("IAVD1   \n");
-    rtm.sendMessage("switchにしてみる!", myHouseGroup);
+    rtm.sendMessage("switchにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*キャスト.*/)) {
+  if (message.match(/.*キャスト.*/)) {
     //inputModeInterval(4);
     sendSignalToTV("IAVD2   \n");
-    rtm.sendMessage("ChromeCastにしてみる!", myHouseGroup);
+    rtm.sendMessage("ChromeCastにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-  if (message.text.match(/.*(pc)|(PC).*/)) {
+  if (message.match(/.*(pc)|(PC).*/)) {
     //inputModeInterval(4);
     sendSignalToTV("IAVD3   \n");
-    rtm.sendMessage("PCにしてみる!", myHouseGroup);
+    rtm.sendMessage("PCにしてみる!", myHouseGroupId);
     await sleep(1000);
   }
-};
-
-rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
-  console.log("Reaction added:", reaction);
-});
-
-rtm.on(RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(
-  reaction
-) {
-  console.log("Reaction removed:", reaction);
-});
-
-let irRequestPost = ir => {
-  let irStr = JSON.stringify(ir);
-  let options = {
-    host: secret.irKitIP(),
-    path: "/messages",
-    post: 80,
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": irStr.length,
-      "X-Requested-With": "curl"
-    },
-    method: "POST",
-    json: true
-  };
-  console.log(options);
-  let req = http.request(options, res => {
-    res.setEncoding("utf8");
-    if (res.statusCode == 200) {
-      rtm.sendMessage("成功!", myHouseGroup);
-    } else {
-      rtm.sendMessage("失敗しちゃった...", myHouseGroup);
-    }
-  });
-
-  req.on("error", e => {
-    console.log("problem with request: " + e.message);
-    rtm.sendMessage("失敗しちゃった...", myHouseGroup);
-  });
-
-  req.write(irStr);
-  req.end();
-};
-
-let inputModeInterval = function(times) {
-  var count = 0;
-  var id = setInterval(function() {
-    irRequestPost(irFreq.tvInputModeChange());
-    count += 1;
-    if (times <= count) {
-      clearInterval(id); //idをclearIntervalで指定している
-    }
-  }, 1500);
 };
 
 rtm.start();
